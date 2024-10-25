425ad73bc2223af3132a1ed134c84498dc8e24a7
Иван <ivanperegudov2004@gmail.com>
2024-10-25 23:00:08 +0600

    Full

--- CoffeMAchine.Tests/CoffeeMachineTest.cs
+++ CoffeMAchine.Tests/CoffeeMachineTest.cs
@@ -6,13 +6,12 @@ using System;
 namespace CoffeMachine.Tests
 {
     public class CoffeeMachineTest
-        //Доделать тесты по CoffeeMachine Get...
     {
         [Fact]
         public void LoadMilk_Success()
         {
             //Arrange
-            int expectedMilk = 8;
+            int expectedMilk = 50;
             //Act
             var loadMilk = new CoffeeMachine().LoadMilk(expectedMilk);
             //Assert
@@ -28,7 +27,6 @@ namespace CoffeMachine.Tests
 
 
         }
-
 
         [Fact]
         public void LoadWater_Success()
@@ -68,6 +66,94 @@ namespace CoffeMachine.Tests
             //Act + Assert
             Assert.Throws<ArgumentException>(() => coffeeMachine.LoadBeans(101));
         }
+        [Fact]
+        public void GetMilkLevel_Success()
+        {
+            //Arrange
+            var coffeeMachine = new CoffeeMachine();
+            int initialMilk = coffeeMachine.GetMilkLevel(); 
+            int addedMilk = 100;
+            coffeeMachine.LoadMilk(addedMilk);
+
+            //Act
+            int actualMilk = coffeeMachine.GetMilkLevel();
+
+            //Assert
+            Assert.Equal(initialMilk + addedMilk, actualMilk);
+        }
+        [Fact]
+        public void GetWaterLevel_Success()
+        {
+            //Arrange
+            var coffeeMachine = new CoffeeMachine();
+            int initialWater = coffeeMachine.GetWaterLevel();
+            int addedWater = 100;
+            coffeeMachine.LoadWater(addedWater);
+
+            //Act
+            int actualWater = coffeeMachine.GetWaterLevel();
+
+            //Assert
+            Assert.Equal(initialWater + addedWater, actualWater);
+        }
+        [Fact]
+        public void GetBeansLevel_Success()
+        {
+            //Arrange
+            var coffeeMachine = new CoffeeMachine();
+            int initialBeans = coffeeMachine.GetBeansLevel();
+            int addedBeans = 100;
+            coffeeMachine.LoadBeans(addedBeans);
+
+            //Act
+            int actualBeans = coffeeMachine.GetBeansLevel();
+
+            //Assert
+            Assert.Equal(initialBeans + addedBeans, actualBeans);
+        }
+        [Fact]
+        public void Brew_Cappuccino_Success()
+        {
+            //Arrange
+            var coffeeMachine = new CoffeeMachine();
+            coffeeMachine.LoadMilk(50);
+            coffeeMachine.LoadBeans(50);
+            coffeeMachine.LoadWater(50);
+            //Act
+            var coffee = coffeeMachine.Brew(RecipeNames.Cappuccino);
+            //Assert
+            Assert.NotNull(coffee);
+            Assert.Equal(RecipeNames.Cappuccino, coffee.Recipe);
+        }
+        [Fact]
+        public void Brew_Filtered_Success()
+        {
+            //Arrange
+            var coffeeMachine = new CoffeeMachine();
+            coffeeMachine.LoadMilk(50);
+            coffeeMachine.LoadBeans(50);
+            coffeeMachine.LoadWater(50);
+            //Act
+            var coffee = coffeeMachine.Brew(RecipeNames.Filtered);
+            //Assert
+            Assert.NotNull(coffee);
+            Assert.Equal(RecipeNames.Filtered, coffee.Recipe);
+        }
+        [Fact]
+        public void Brew_Espresso_Success()
+        {
+            //Arrange
+            var coffeeMachine = new CoffeeMachine();
+            coffeeMachine.LoadMilk(50);
+            coffeeMachine.LoadBeans(50);
+            coffeeMachine.LoadWater(50);
+            //Act
+            var coffee = coffeeMachine.Brew(RecipeNames.Espresso);
+            //Assert
+            Assert.NotNull(coffee);
+            Assert.Equal(RecipeNames.Espresso, coffee.Recipe);
+        }
+
     }
 
     
