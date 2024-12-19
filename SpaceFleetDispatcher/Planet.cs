536be350fcd4102c42014dc2ae3c323f6d12539f
ivan <ivanperegudov2004@gmail.com>
2024-12-19 10:13:09 +0600

    Добавил отправную точку корабля(Земля)

--- SpaceFleetDispatcher/Planet.cs
+++ SpaceFleetDispatcher/Planet.cs
@@ -7,9 +7,12 @@
         public double Y { get; set; }
         public double CargoDemand { get; set; }
 
+        private readonly double EarthX = 1.0;
+        private readonly double EarthY = -0.4;
+
         public double CalculateDistance()
         {
-            return Math.Sqrt(X * X + Y * Y);
+            return Math.Sqrt(Math.Pow(X - EarthX, 2) + Math.Pow(Y - EarthY, 2));
         }
 
     }
